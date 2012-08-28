44b7827959e6aaf4933eb23b9f858d9782b613d0
Danny Guo <dannyguo91@gmail.com>
2012-08-28 01:50:21 -0400

    added RSS urls

--- test.py
+++ test.py
@@ -6,11 +6,22 @@ urls['The New York Times'] = 'http://feeds.nytimes.com/nyt/rss/HomePage'
 urls['ESPN'] = 'http://sports.espn.go.com/espn/rss/news'
                 
 
-url = urls['ESPNd']
-#except KeyError, e:
- #   url = None
-                
-if url is None:
-    print None
-else:
-    print feedparser.parse(url)
+url = 'http://www.gamespot.com/rss/game_updates.php'
+
+#url = 'http://feeds.abcnews.com/abcnews/topstories'
+
+feed = feedparser.parse(url)
+
+
+i = 0
+for item in feed['items']:
+    if i == 0:
+        for key in item:
+            print key
+        print item['title']
+        print item['link']
+        print item['published']
+        for key in item['summary_detail']:
+            print key
+        print item['summary_detail']['value']
+        i = 2
